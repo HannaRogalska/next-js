f31c3cb0e0ab0d5e99562fcc0410f58e75de724a
Hanna Rogalska <hanrogalska@gmail.com>
2026-02-18 12:50:59 +0100

    form action

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,58 @@
-
+type Todo = {
+  id: string,
+  title: string ;
+  content: string;
+};
+let todo: Todo[] = [
+  { title: "First", content: "Post 1", id: "1"  },
+  { title: "Second", content: "Post 2", id: "2" },
+];
 
 export default function Home() {
+  const createPost = async (formData: FormData) => {
+    "use server";
+    const title = formData.get("title")?.toString() || "";
+    const content = formData.get("content")?.toString() || "";
+    const id = formData.get("id")?.toString() || "";
+    todo.push({ title, content, id });
+    console.log(todo);
+  };
   return (
-    <h1>My first next js app</h1>
+    <div>
+      <form action={createPost}>
+        <input
+          type="text"
+          name="title"
+          placeholder="Title"
+          className="border"
+        />
+        <input
+          type="text"
+          name="content"
+          placeholder="Content"
+          className="border"
+        />
+        <input
+          type="number"
+          name="id"
+          placeholder="ID"
+          className="border"
+        />
+        <button type="submit" className="border">
+          Create
+        </button>
+      </form>
+
+      <div>
+        {todo.map((el) => {
+          return (
+            <div key={el.id}>
+              <h1>{el.title}</h1>
+              <div>{el.content}</div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
   );
 }
