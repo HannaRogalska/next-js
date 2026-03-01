711620d1a564796a494a120b5eb58b46a24ff24c
Hanna Rogalska <hanrogalska@gmail.com>
2026-03-01 19:49:35 +0100

    blank

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,41 +1,4 @@
 
-import { getTodos, deletePost } from "./actions/formAction";
-import { alertFunction } from "@/helpers/helpers";
-
-
 export default async function Home() {
-  const todo = await getTodos()
-  
- 
- await new Promise((r) => setTimeout(r, 3000));
-  return (
-    <div>
-      <form action={alertFunction}>
-        <input
-          type="text"
-          name="title"
-          placeholder="Title"
-          className="border"
-        />
-        <button type="submit" className="border">
-          Create
-        </button>
-      </form>
-      <div>
-        {todo.map((el) => {
-          return (
-            <div key={el.id}>
-              <h1>{el.title}</h1>
-              <form action={deletePost}>
-                <input type="hidden" name="id" value={el.id} />
-                <button type="submit" className="border">
-                  delete
-                </button>
-              </form>
-            </div>
-          );
-        })}
-      </div>
-    </div>
-  );
+  return <div>Hello</div>;
 }
--- src/types/todo.ts
+++
@@ -1,6 +0,0 @@
-export type Todos = {
-  userId: number;
-  id: number;
-  title: string;
-  completed: boolean;
-};
